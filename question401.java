e320b511cfab4ec58f644f3f8cf1982ce8a02edb
lll <lvliangbupt@163.com>
2017-07-21 16:58:28 +0800

    401

---
+++ question401.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package one;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* @author lvliangliang E-mail:lvliangbupt@136.com
+* @version Create time：2017年7月21日 上午10:49:06
+* class illustration:
+*/
+/**
+ * @author NewUser
+ *
+ */
+public class question401 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public List<String> readBinaryWatch(int num) {
+		int hournum=0;
+		int minnum=0;
+		String [][]library;
+		List<String> result=new ArrayList<String>();
+		if(num<=4){
+			for(hournum=0;hournum<num;num++){
+				switch (hournum){
+				case 0:
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					result.add("");
+					break;
+				
+				}
+			}
+		}else if(num<=6){
+			
+		}else{
+			
+		}
+		return result;
+	}
+
+}
