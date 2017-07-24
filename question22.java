adee266f8da02280ec7a7a4490de6d4569dadad0
lll <lvliangbupt@163.com>
2017-07-24 13:42:01 +0800

    leetcode 22

--- question22.java
+++ question22.java
@@ -4,6 +4,7 @@
 package one;
 
 import java.util.List;
+import java.util.Vector;
 
 /**
 * @author lvliangliang E-mail:lvliangbupt@136.com
@@ -18,21 +19,28 @@ public class question22 {
 
 	/**
 	 * @param args
+	 * 这也是递归，只不过没有return 来作为结束，这里是通过运行完方法中的语句来结束的。
+	 * 刚开始之所以看不出来回溯的原因是因为上述的原因
+	 * 这种可以说是隐式的回溯很哟意思，
+	 * 不用面向过程去想，主要想好了方法，只需几句代码就可以完成非常复杂的过程。
+	 * 这就是回溯的魅力。
+	 * 几个if语句就可以回溯，自动的结束这一种情况就会回溯一步进行下一种情况。	
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String> a=generateParenthesis(4);
+		System.out.println(a.size());
+		for(int i=0;i<a.size();i++)
+			System.out.println(a.get(i));
 	}
-	public List<String> generateParenthesis(int n){
-		
-		
-		
-		
-		
-		
-		
-		return null;
-		
+	public static List<String> generateParenthesis(int n){
+		Vector<String> result=new Vector<String>();
+		String temp ="";
+		work(result,n,n,temp);
+		return result;
 	}
-
+	private static void work(Vector<String> result, int l, int r, String temp) {
+		if(l==0&&r==0) result.add(temp);
+		if(l>0)work(result,l-1,r,temp+"(");
+		if(r>0&&l<r)work(result,l,r-1,temp+")");
+	}
 }
